7a1ca063b50276e6b06d5f82a31103aefffc1f2b
Sreeja Pullagura <sreeja.pullagura@teachforamerica.org>
2019-01-31 16:46:11 -0500

    adding enable clr security 0

--- docker/mssqlssis/enable_clr_integration.sql
+++ docker/mssqlssis/enable_clr_integration.sql
@@ -2,6 +2,10 @@ sp_configure 'show advanced options', 1;
 GO  
 RECONFIGURE;  
 GO  
+EXEC sp_configure 'clr strict security',0;
+GO
+RECONFIGURE;
+GO
 sp_configure 'clr enabled', 1;  
 GO  
 RECONFIGURE;  
