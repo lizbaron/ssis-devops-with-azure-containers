8aa9aef7929698306613ebe0ac8df0fef70fe85b
Sreeja Pullagura <Sreeja.Pullagura@teachforamerica.org>
2019-10-09 09:37:26 -0400

    fixing change login script

--- docker/mssqlssis/changeSqlLogin.ps1
+++ docker/mssqlssis/changeSqlLogin.ps1
@@ -5,6 +5,6 @@ param(
 
 if($null -ne $sa_password -And $sa_password -ne "") {
 	Write-Verbose "Changing SA login credentials"
-	$sqlcmd = "ALTER LOGIN sa with password=$sa_password,CHECK_POLICY=OFF,CHECK_EXPIRATION=OFF" + ";ALTER LOGIN sa ENABLE;"
+	$sqlcmd = "ALTER LOGIN sa with password="+$sa_password+",CHECK_POLICY=OFF,CHECK_EXPIRATION=OFF" + ";ALTER LOGIN sa ENABLE;"
 	& sqlcmd -Q $sqlcmd
 }
